b9e5fcb1940a0e7d2172a40057405d1705ed8633
Tatyana Korosteleva <tvkorosteleva@avito.ru>
2017-02-17 16:23:19 +0300

    #! refactor responsive svg;

---
+++ range-controller.js
@@ -0,0 +1,16 @@
+(function () {
+    let rangeNode = document.querySelector('.js-iframe-width-range');
+    let frameNode = document.querySelector('.js-iframe');
+    let initRangeValue = parseInt(rangeNode.getAttribute('max'), 10);
+    rangeNode.setAttribute('value', initRangeValue);
+    setIframeWidth(initRangeValue);
+
+    rangeNode.addEventListener('input', function (event) {
+        let rangeValue = event.target.value;
+        setIframeWidth(rangeValue);
+    });
+
+    function setIframeWidth(rangeValue) {
+        frameNode.style.setProperty('--iframe-width-diff', rangeValue + 'px');
+    }
+}());
